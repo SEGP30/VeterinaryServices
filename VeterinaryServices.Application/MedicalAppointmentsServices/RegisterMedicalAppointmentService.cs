ecbd64b55f5759c980811daa21e3397cc2e3d800
Sebastián Guarín <mothder@outlook.com>
2021-07-22 19:50:09 -0500

    Register service added

--- VeterinaryServices.Application/MedicalAppointmentsServices/RegisterMedicalAppointmentService.cs
+++ VeterinaryServices.Application/MedicalAppointmentsServices/RegisterMedicalAppointmentService.cs
@@ -1,4 +1,10 @@
-﻿using VeterinaryServices.Domain.Contracts;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using VeterinaryServices.Application.MedicalAppointmentsServices.Requests;
+using VeterinaryServices.Application.MedicalAppointmentsServices.Responses;
+using VeterinaryServices.Domain.Contracts;
+using VeterinaryServices.Domain.Entities;
 
 namespace VeterinaryServices.Application.MedicalAppointmentsServices
 {
@@ -10,8 +16,43 @@ namespace VeterinaryServices.Application.MedicalAppointmentsServices
         {
             _unitOfWork = unitOfWork;
         }
-        
-        
+
+        public async Task<RegisterMedicalAppointmentResponse> Execute(RegisterMedicalAppointmentRequest request)
+        {
+            
+            var clientInDb = await _unitOfWork.ClientRepository.Find(request.OwnerId);
+            if (clientInDb == null)
+                return null;
+            
+            var clientPets = await _unitOfWork.PetRepository.FindBy(pet => pet.OwnerId == clientInDb.Id);
+            var petRegistered = clientPets.FirstOrDefault(pet => pet.Name == request.PetName && pet.Gender == request.PetGender && pet.Color == request.PetColor);
+
+            if (petRegistered == null)
+                return null;
+            
+            var doctorInDb = await _unitOfWork.DoctorRepository.Find(request.DoctorId);
+            if (doctorInDb == null)
+                return null;
+
+            var medicalAppointmentToRegister = new MedicalAppointment
+            {
+                Cost = request.Cost,
+                CreationDate = DateTime.Now,
+                Date = DateTime.Today,
+                Observation = request.Observation
+            };
+            
+            medicalAppointmentToRegister.Input(petRegistered, doctorInDb);
+            
+            await _unitOfWork.MedicalAppointmentRepository.Insert(medicalAppointmentToRegister);
+            await _unitOfWork.Commit();
+
+            return new RegisterMedicalAppointmentResponse
+            {
+                Message = "Cita médica registrada satisfactoriamente"
+            };
+
+        }
         
     }
 }
--- VeterinaryServices.Application/MedicalAppointmentsServices/Requests/RegisterMedicalAppointmentRequest.cs
+++ VeterinaryServices.Application/MedicalAppointmentsServices/Requests/RegisterMedicalAppointmentRequest.cs
@@ -1,8 +1,21 @@
-﻿namespace VeterinaryServices.Application.MedicalAppointmentsServices.Requests
+﻿using System;
+using VeterinaryServices.Domain.Enums;
+
+namespace VeterinaryServices.Application.MedicalAppointmentsServices.Requests
 {
     public class RegisterMedicalAppointmentRequest
     {
         public long OwnerId { get; set; }
+        public string PetName { get; set; }
+        public Gender PetGender { get; set; }
+        public string PetColor { get; set; }
+        public double Cost { get; set; }
+        public string Observation { get; set; }
+        
+        public long DoctorId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime Date { get; set; }
+        
         
     }
 }
