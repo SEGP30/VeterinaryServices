aeeb6f70ee5685c6362375c13cd61589ef3610a6
Sebastián Guarín <mothder@outlook.com>
2021-08-06 13:17:02 -0500

    Register aesthetic services added

---
+++ VeterinaryServices.Application/AestheticServices_Services/RegisterAestheticService_Service.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using VeterinaryServices.Application.AestheticServices_Services.Requests;
+using VeterinaryServices.Application.AestheticServices_Services.Responses;
+using VeterinaryServices.Domain.Contracts;
+using VeterinaryServices.Domain.Entities;
+
+namespace VeterinaryServices.Application.AestheticServices_Services
+{
+    public class RegisterAestheticService_Service
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RegisterAestheticService_Service(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<RegisterAestheticServiceResponse> Execute(RegisterAestheticServiceRequest request)
+        {
+            var clientInDb = await _unitOfWork.ClientRepository.Find(request.OwnerId);
+            if (clientInDb == null)
+                return null;
+            
+            var clientPets = await _unitOfWork.PetRepository.FindBy(pet => pet.OwnerId == clientInDb.Id);
+            var petRegistered = clientPets.FirstOrDefault(pet => pet.Name == request.PetName && pet.Gender == request.PetGender && pet.Color == request.PetColor);
+
+            if (petRegistered == null)
+                return null;
+            
+            var doctorInDb = await _unitOfWork.DoctorRepository.Find(request.DoctorId);
+            if (doctorInDb == null)
+                return null;
+
+            var aestheticServiceToRegister = new AestheticService
+            {
+                Cost = request.Cost,
+                CreationDate = DateTime.Now,
+                Date = DateTime.Today
+            };
+            
+            aestheticServiceToRegister.Input(petRegistered, doctorInDb);
+
+            await _unitOfWork.AestheticServiceRepository.Insert(aestheticServiceToRegister);
+            await _unitOfWork.Commit();
+
+            return new RegisterAestheticServiceResponse
+            {
+                Message = "Servicio estético registrado satisfactoriamente"
+            };
+
+        }
+        
+    }
+}
---
+++ WebApi/Controllers/AestheticServicesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VeterinaryServices.Application.AestheticServices_Services;
+using VeterinaryServices.Application.AestheticServices_Services.Requests;
+using VeterinaryServices.Domain.Contracts;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("aesthetic_Services")]
+    public class AestheticServicesController: ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AestheticServicesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RegisterAesthethicService(RegisterAestheticServiceRequest request)
+        {
+            var service = new RegisterAestheticService_Service(_unitOfWork);
+            var response = await service.Execute(request);
+            return Ok(response);
+        }
+    }
+}
